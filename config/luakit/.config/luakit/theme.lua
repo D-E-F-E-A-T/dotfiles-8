7a0e52a243de659155c87cb70cef0459f31bd7ee
mohabaks <mohabaks64@gmail.com>
2017-11-01 22:27:14 +0300

    Added minor changes for luakit theme.lua

--- config/luakit/.config/luakit/theme.lua
+++ config/luakit/.config/luakit/theme.lua
@@ -5,81 +5,81 @@
 local theme = {}
 
 -- Default settings
-theme.font = "11px Iosevka Slab"
-theme.fg   = "#586e75"
-theme.bg   = "#dfddd5"
+theme.font = "11px Neep"
+theme.fg   = "#b9b9b9"
+theme.bg   = "#101010"
 
 -- Genaral colours
-theme.success_fg = "#586e75"
-theme.loaded_fg  = "#586e75"
-theme.error_fg = "#dc322f"
-theme.error_bg = "#dfddd5"
+theme.success_fg = "#b9b9b9"
+theme.loaded_fg  = "#b9b9b9"
+theme.error_fg = "#038d6c"
+theme.error_bg = "#101010"
 
 -- Warning colours
-theme.warning_fg = "#5d7b5c"
-theme.warning_bg = "#586e75"
+theme.warning_fg = "#8e8e8e"
+theme.warning_bg = "#b9b9b9"
 
 -- Notification colours
-theme.notif_fg = "#586e75"
-theme.notif_bg = "#dfddd5"
+theme.notif_fg = "#b9b9b9"
+theme.notif_bg = "#101010"
 
 -- Menu colours
-theme.menu_fg                   = "#586e75"
-theme.menu_bg                   = "#c5c5c5"
-theme.menu_selected_fg          = "#dfddd5"
-theme.menu_selected_bg          = "#dc322f"
-theme.menu_title_bg             = "#dfddd5"
-theme.menu_primary_title_fg     = "#534475"
-theme.menu_secondary_title_fg   = "#aa6772"
+theme.menu_fg                   = "#b9b9b9"
+theme.menu_bg                   = "#151515"
+theme.menu_selected_fg          = "#f7f7f7"
+theme.menu_selected_bg          = "#038d6c"
+theme.menu_title_bg             = "#101010"
+theme.menu_primary_title_fg     = "#686868"
+theme.menu_secondary_title_fg   = "#8e8e8e"
 
-theme.menu_disabled_fg = "#999"
+theme.menu_disabled_fg = "#404040"
 theme.menu_disabled_bg = theme.menu_bg
 theme.menu_enabled_fg = theme.menu_fg
 theme.menu_enabled_bg = theme.menu_bg
-theme.menu_active_fg = "#060"
+theme.menu_active_fg = "#686868"
 theme.menu_active_bg = theme.menu_bg
 
 -- Proxy manager
-theme.proxy_active_menu_fg      = '#dfddd5'
-theme.proxy_active_menu_bg      = '#586e75'
-theme.proxy_inactive_menu_fg    = '#888'
-theme.proxy_inactive_menu_bg    = '#586e75'
+theme.proxy_active_menu_fg      = '#f7f7f7'
+theme.proxy_active_menu_bg      = '#038d6c'
+theme.proxy_inactive_menu_fg    = '#404040'
+theme.proxy_inactive_menu_bg    = '#151515'
 
 -- Statusbar specific
-theme.sbar_fg         = "#586e75"
-theme.sbar_bg         = "#c5c5c5"
+theme.sbar_fg         = "#b9b9b9"
+theme.sbar_bg         = "#151515"
 
 -- Downloadbar specific
-theme.dbar_fg         = "#586e75"
-theme.dbar_bg         = "#dc322f"
-theme.dbar_error_fg   = "#dc322f"
+theme.dbar_fg         = "#b9b9b9"
+theme.dbar_bg         = "#038d6c"
+theme.dbar_error_fg   = "#038d6c"
 
 -- Input bar specific
-theme.ibar_fg           = "#586e75"
-theme.ibar_bg           = "#dfddd5"
+theme.ibar_fg           = "#b9b9b9"
+theme.ibar_bg           = "#101010"
 
 -- Tab label
-theme.tab_fg            = "#586e75"
-theme.tab_bg            = "#dfddd5"
-theme.tab_hover_bg      = "#268bd2"
-theme.tab_ntheme        = "#816886"
-theme.selected_fg       = "#586e75"
-theme.selected_bg       = "#c5c5c5"
-theme.selected_ntheme   = "#816886"
-theme.loading_fg        = "#586e75"
-theme.loading_bg        = "#dfddd5"
+theme.tab_fg            = "#b9b9b9"
+theme.tab_bg            = "#101010"
+theme.tab_hover_bg      = "#038d6c"
+theme.tab_ntheme        = "#b9b9b9"
+theme.selected_fg       = "#b9b9b9"
+theme.selected_bg       = "#151515"
+theme.selected_ntheme   = "#b9b9b9"
+theme.loading_fg        = "#b9b9b9"
+theme.loading_bg        = "#101010"
 
 theme.selected_private_tab_bg = "#3d295b"
 theme.private_tab_bg    = "#22254a"
 
 -- Trusted/untrusted ssl colours
-theme.trust_fg          = "#5d7b5c"
-theme.notrust_fg        = "#dc322f"
+theme.trust_fg          = "#038d6c"
+theme.notrust_fg        = "#404040"
 
 -- General colour pairings
-theme.ok = { fg = "#586e75", bg = "#dfddd5" }
-theme.warn = { fg = "#5d7b5c", bg = "#dfddd5" }
-theme.error = { fg = "#dc322f", bg = "#dfddd5" }
+theme.ok = { fg = "#b9b9b9", bg = "#101010" }
+theme.warn = { fg = "#5d7b5c", bg = "#101010" }
+theme.error = { fg = "#038d6c", bg = "#101010" }
 
 return theme
 

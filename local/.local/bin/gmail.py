3cf75026492db61b1c210823f84725f34ce67e1b
mohabaks <mohabaks64@gmail.com>
2018-04-22 22:08:30 +0300

    Initial Gentoo Minimalistic Look

--- local/.local/bin/gmail.py
+++ local/.local/bin/gmail.py
@@ -1,10 +1,10 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 from urllib.request import FancyURLopener
 from accounts import accounts
 
-email = accounts.student_email 
-password = accounts.student_pass
+email = accounts.email
+password = accounts.password
 
 try:
     url = 'https://%s:%s@mail.google.com/mail/feed/atom' % (email, password)
@@ -23,4 +23,3 @@ else:
     fullcount = contents[ifrom:ito]
 
     print(fullcount)
-
